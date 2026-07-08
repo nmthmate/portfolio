8df5faf4a143965e3c07ffb428377cbe45952603
Németh Máté <matepataky@gmail.com>
2026-07-08 15:25:21 +0200

    Enable hash routing for GitHub Pages subfolder

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,13 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { LocationStrategy, HashLocationStrategy } from '@angular/common';
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
+    provideRouter(routes),
+    { provide: LocationStrategy, useClass: HashLocationStrategy }
   ]
 };
